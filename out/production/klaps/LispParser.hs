7f70073d89202d9b896da935ea3b700b861d2e4e
khang00 <dlqkhang8@gmail.com>
2021-01-17 21:55:25 +0700

    add function

--- out/production/klaps/LispParser.hs
+++ out/production/klaps/LispParser.hs
@@ -1,66 +1,68 @@
 module LispParser where
 
-import Text.ParserCombinators.Parsec hiding (spaces)
-import Numeric (readHex, readOct, readFloat)
-import Data.Ratio ((%))
-import Data.Complex (Complex (..))
 import Control.Monad (liftM)
-import LispVal
 import Control.Monad.Error.Class (throwError)
+import Data.Complex (Complex (..))
+import Data.Ratio ((%))
+import LispVal
+import Numeric (readFloat, readHex, readOct)
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parseLisp "lisp" input of
-    Left err -> throwError $ Parser err
-    Right val -> return val
+  Left err -> throwError $ Parser err
+  Right val -> return val
 
 parseLisp :: SourceName -> String -> Either ParseError LispVal
 parseLisp = parse parseExpr
 
 parseExpr :: Parser LispVal
-parseExpr =  parseAtom
-         <|> parseString
-         <|> parseCharacter
-         <|> try parseFloat
-         <|> try parseRational
-         <|> try parseComplex
-         <|> parseNumber
-         <|> parseQuote
-         <|> parseQuasiquote
-         <|> do _ <- char '('
-                x <- try parseList <|> parseDottedList
-                _ <- char ')'
-                return x
+parseExpr =
+  parseAtom
+    <|> parseString
+    <|> parseCharacter
+    <|> try parseFloat
+    <|> try parseRational
+    <|> try parseComplex
+    <|> parseNumber
+    <|> parseQuote
+    <|> parseQuasiquote
+    <|> do
+      _ <- char '('
+      x <- try parseList <|> parseDottedList
+      _ <- char ')'
+      return x
 
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
 
 parseDottedList :: Parser LispVal
 parseDottedList = do
-                    x <- endBy parseExpr spaces
-                    xs <- char '.' >> spaces >> parseExpr
-                    return $ DottedList x xs
+  x <- endBy parseExpr spaces
+  xs <- char '.' >> spaces >> parseExpr
+  return $ DottedList x xs
 
 parseQuote :: Parser LispVal
 parseQuote = do
-    _ <- char '\''
-    x <- parseExpr
-    return $ List [Atom "quote", x]
+  _ <- char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
 
 parseQuasiquote :: Parser LispVal
 parseQuasiquote = do
-                    _ <- char ','
-                    x <- parseExpr
-                    return $ List [Atom "quasiquote", x]
+  _ <- char ','
+  x <- parseExpr
+  return $ List [Atom "quasiquote", x]
 
 parseAtom :: Parser LispVal
 parseAtom = do
-              first <- letter <|> symbol
-              rest <- many (letter <|> digit <|> symbol)
-              let atom = first:rest
-              return $ case atom of
-                         "True" -> Bool True
-                         "False" -> Bool False
-                         _    -> Atom atom
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "True" -> Bool True
+    "False" -> Bool False
+    _ -> Atom atom
 
 parseNumber :: Parser LispVal
 parseNumber = parseDecimal <|> parseHex <|> parseOct <|> parseDecimalNotation
@@ -69,70 +71,73 @@ parseDecimal :: Parser LispVal
 parseDecimal = many1 digit >>= (return . Number . read)
 
 parseDecimalNotation :: Parser LispVal
-parseDecimalNotation = do _ <- try $ string "#d"
-                          x <- many1 digit
-                          (return . Number . read) x
+parseDecimalNotation = do
+  _ <- try $ string "#d"
+  x <- many1 digit
+  (return . Number . read) x
 
 parseHex :: Parser LispVal
-parseHex = do _ <- try $ string "#x"
-              x <- many1 hexDigit
-              return $ Number (hexToDigit x)
+parseHex = do
+  _ <- try $ string "#x"
+  x <- many1 hexDigit
+  return $ Number (hexToDigit x)
 
 hexToDigit :: (Eq a, Num a) => String -> a
 hexToDigit x = fst $ head (readHex x)
 
 parseOct :: Parser LispVal
-parseOct = do _ <- try $ string "#o"
-              x <- many1 octDigit
-              return $ Number (octToDigit x)
+parseOct = do
+  _ <- try $ string "#o"
+  x <- many1 octDigit
+  return $ Number (octToDigit x)
 
 octToDigit :: (Eq a, Num a) => String -> a
 octToDigit x = fst $ head (readOct x)
 
 parseString :: Parser LispVal
 parseString = do
-                _ <- char '"'
-                x <- many $ noneOf "\"\\" <|> escapedChars
-                _ <- char '"'
-                return $ String x
+  _ <- char '"'
+  x <- many $ noneOf "\"\\" <|> escapedChars
+  _ <- char '"'
+  return $ String x
 
 parseFloat :: Parser LispVal
 parseFloat = do
-                whole <- many1 digit
-                _ <- char '.'
-                frac <- many1 digit
-                return . Float . fst . head . readFloat $ whole ++ "." ++ frac
+  whole <- many1 digit
+  _ <- char '.'
+  frac <- many1 digit
+  return . Float . fst . head . readFloat $ whole ++ "." ++ frac
 
 parseRational :: Parser LispVal
 parseRational = do
-                   numerator <- many1 digit
-                   _ <- char '/'
-                   denominator <- many1 digit
-                   return $ Rational (read numerator % read denominator)
+  numerator <- many1 digit
+  _ <- char '/'
+  denominator <- many1 digit
+  return $ Rational (read numerator % read denominator)
 
 parseComplex :: Parser LispVal
 parseComplex = do
-                  real <- many1 digit
-                  _ <- char '+'
-                  imaginary <- many1 digit
-                  _ <- char 'i'
-                  return $ Complex (read real :+ read imaginary)
+  real <- many1 digit
+  _ <- char '+'
+  imaginary <- many1 digit
+  _ <- char 'i'
+  return $ Complex (read real :+ read imaginary)
 
 parseCharacter :: Parser LispVal
 parseCharacter = try (string "\\#") >>= (return . String)
 
 escapedChars :: Parser Char
-escapedChars = do _ <- char '\\'
-                  x <- oneOf "\\\"\'nrt"
-                  return $ case x of
-                    '\\' -> x
-                    '"' -> x
-                    '\'' -> x
-                    'n' -> '\n'
-                    't' -> '\t'
-                    'r' -> '\r'
-                    _ -> x
-
+escapedChars = do
+  _ <- char '\\'
+  x <- oneOf "\\\"\'nrt"
+  return $ case x of
+    '\\' -> x
+    '"' -> x
+    '\'' -> x
+    'n' -> '\n'
+    't' -> '\t'
+    'r' -> '\r'
+    _ -> x
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=>?@^_~"
--- src/LispVal.hs
+++ src/LispVal.hs
@@ -1,26 +1,32 @@
 module LispVal where
 
+import Control.Monad.Cont (liftIO, liftM)
+import Control.Monad.Error.Class (MonadError, catchError)
+import Control.Monad.Except (ExceptT, runExceptT, throwError)
 import Data.Complex (Complex)
-import Control.Monad.Except (ExceptT, throwError, runExceptT)
-import Control.Monad.Error.Class (catchError, MonadError)
-import Text.ParserCombinators.Parsec (ParseError)
-import Control.Monad.Cont (liftM, liftIO)
 import Data.IORef
+import Text.ParserCombinators.Parsec (ParseError)
 
 instance Show LispVal where show = showVal
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | Float Float
-             | Rational Rational
-             | Complex (Complex Float)
-             | String String
-             | Character String
-             | Bool Bool
-             | PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
-             | Func { params :: [String], vararg :: Maybe String,
-                      body :: [LispVal], closure :: Environment }
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | Float Float
+  | Rational Rational
+  | Complex (Complex Float)
+  | String String
+  | Character String
+  | Bool Bool
+  | PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
+  | Func
+      { params :: [String],
+        vararg :: Maybe String,
+        body :: [LispVal],
+        closure :: Environment
+      }
 
 showVal :: LispVal -> String
 showVal (List contents) = "(" ++ wordsList contents ++ ")"
@@ -34,16 +40,36 @@ showVal (Complex contents) = show contents
 showVal (Character contents) = show contents
 showVal (Bool True) = "True"
 showVal (Bool False) = "False"
+showVal (PrimitiveFunc _) = "<primitive>"
+showVal (Func {params = args, vararg = varargs, body = _, closure = _}) =
+  "(lambda (" ++ unwords (map show args)
+    ++ ( case varargs of
+           Nothing -> ""
+           Just arg -> " . " ++ arg
+       )
+    ++ ") ...)"
+
+type IOThrowsError = ExceptT LispError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+runIOThrows :: IOThrowsError String -> IO String
+runIOThrows action = runExceptT (trapError action) >>= return . extractValue
 
 type ThrowsError = Either LispError
+
 instance Show LispError where show = showError
-data LispError = NumArgs Integer [LispVal]
-               | TypeMismatch String LispVal
-               | Parser ParseError
-               | BadSpecialForm String LispVal
-               | NotFunction String String
-               | UnboundVar String String
-               | Default String
+
+data LispError
+  = NumArgs Integer [LispVal]
+  | TypeMismatch String LispVal
+  | Parser ParseError
+  | BadSpecialForm String LispVal
+  | NotFunction String String
+  | UnboundVar String String
+  | Default String
 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
@@ -52,59 +78,63 @@ trapError :: (MonadError a m, Show a) => m String -> m String
 trapError action = catchError action (return . show)
 
 showError :: LispError -> String
-showError (UnboundVar message varName)  = message ++ ": " ++ varName
+showError (UnboundVar message varName) = message ++ ": " ++ varName
 showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func)    = message ++ ": " ++ show func
-showError (NumArgs expected found)      = "Expected " ++ show expected
-                                       ++ " args; found values " ++ wordsList found
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
-                                       ++ ", found " ++ show found
-showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (NumArgs expected found) =
+  "Expected " ++ show expected
+    ++ " args; found values "
+    ++ wordsList found
+showError (TypeMismatch expected found) =
+  "Invalid type: expected " ++ expected
+    ++ ", found "
+    ++ show found
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
 showError _ = "Error"
 
 wordsList :: [LispVal] -> String
 wordsList = unwords . map showVal
 
 type Environment = IORef [(String, IORef LispVal)]
-type IOThrowsError = ExceptT LispError IO
 
 getVar :: Environment -> String -> IOThrowsError LispVal
-getVar envRef var  =  do env <- liftIO $ readIORef envRef
-                         maybe (throwError $ UnboundVar "Getting an unbound variable" var)
-                               (liftIO . readIORef)
-                               (lookup var env)
+getVar envRef var = do
+  env <- liftIO $ readIORef envRef
+  maybe
+    (throwError $ UnboundVar "Getting an unbound variable" var)
+    (liftIO . readIORef)
+    (lookup var env)
 
 setVar :: Environment -> String -> LispVal -> IOThrowsError LispVal
-setVar envRef var value = do env <- liftIO $ readIORef envRef
-                             maybe (throwError $ UnboundVar "Setting an unbound variable" var)
-                                   (liftIO . (flip writeIORef value))
-                                   (lookup var env)
-                             return value
+setVar envRef var value = do
+  env <- liftIO $ readIORef envRef
+  maybe
+    (throwError $ UnboundVar "Setting an unbound variable" var)
+    (liftIO . (flip writeIORef value))
+    (lookup var env)
+  return value
 
 defineVar :: Environment -> String -> LispVal -> IOThrowsError LispVal
-defineVar envRef var value = do alreadyDefined <- liftIO $ isBound envRef var
-                                if alreadyDefined
-                                  then setVar envRef var value >> return value
-                                  else liftIO $ do valueRef <- newIORef value
-                                                   env <- readIORef envRef
-                                                   writeIORef envRef ((var, valueRef) : env)
-                                                   return value
+defineVar envRef var value = do
+  alreadyDefined <- liftIO $ isBound envRef var
+  if alreadyDefined
+    then setVar envRef var value >> return value
+    else liftIO $ do
+      valueRef <- newIORef value
+      env <- readIORef envRef
+      writeIORef envRef ((var, valueRef) : env)
+      return value
 
 bindVars :: Environment -> [(String, LispVal)] -> IO Environment
 bindVars envRef binds = readIORef envRef >>= extendEnv binds >>= newIORef
-                        where extendEnv binds' env = liftM (++ env) (mapM addBind binds')
-                              addBind (var, value) = do ref <- newIORef value
-                                                        return (var, ref)
+  where
+    extendEnv binds' env = liftM (++ env) (mapM addBind binds')
+    addBind (var, value) = do
+      ref <- newIORef value
+      return (var, ref)
 
 isBound :: Environment -> String -> IO Bool
 isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
 
 nullEnv :: IO Environment
 nullEnv = newIORef []
-
-liftThrows :: ThrowsError a -> IOThrowsError a
-liftThrows (Left err) = throwError err
-liftThrows (Right val) = return val
-
-runIOThrows :: IOThrowsError String -> IO String
-runIOThrows action = runExceptT (trapError action) >>= return . extractValue
